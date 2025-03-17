f6be36dbae5f3cb7ff8dc112bf13607c8c921697
Marcos Vinícius <marcos.vinicius912142@outlook.com>
2025-03-17 04:22:39 -0300

    faz o css do layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,31 @@
-import {useState} from 'react';
-import './App.css';
-import {Greet, GetAllQuarta, CreateQuartum} from "../wailsjs/go/main/App";
+import { useEffect, useState } from "react";
+import { Greet, GetAllQuarta, CreateQuartum } from "../wailsjs/go/main/App";
+import classes from "./App.module.css";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [resultQuarta, setResultQuarta] = useState([]);
-    const [name, setName] = useState('');
-    const updateName = (e) => setName(e.target.value);
-    const updateResultText = (result) => setResultText(result);
+  const [resultText, setResultText] = useState(
+    "Please enter your name below 👇"
+  );
+  const [resultQuarta, setResultQuarta] = useState([]);
+  const [name, setName] = useState("");
+  const updateName = (e) => setName(e.target.value);
+  const updateResultText = (result) => setResultText(result);
 
-    const updateResultQuarta = (result) => setResultQuarta(result);
+  const updateResultQuarta = (result) => setResultQuarta(result);
 
-    function getAllQuarta (){
-	    	// CreateQuartum()	
-	    	console.log("executando...")
-		const res = GetAllQuarta()
-	    	Promise.resolve(res).then(JSON.parse).then(updateResultQuarta);
-    }
+  function getAllQuarta() {
+    // CreateQuartum()
+    console.log("executando...");
+    const res = GetAllQuarta();
+    Promise.resolve(res).then(JSON.parse).then(updateResultQuarta);
+  }
 
-console.log(resultQuarta)
-    return (
+  useEffect(() => {
+    getAllQuarta();
+  }, []);
+
+  console.log(resultQuarta);
+  /*return (
         <div id="App">
             <div id="result" className="result">{resultText}</div>
             <div id="input" className="input-box">
@@ -31,14 +37,41 @@ console.log(resultQuarta)
 	    <div>
 	    {resultQuarta.map(q => (
 		    <>
-		    <p key={q?.id}>{q?.titulum}</p>
-		    <p key={q?.id}>{q?.pars}</p>
+		    <p className={classes.primary_text} key={`id_${q?.id}`}>{q?.titulum}</p>
+		    <p key={`pars_${q?.id}`}>{
+			    q?.pars === 1 ? "Manhã"
+			    : "Noite"
+		    }</p>
 		    <p key={q?.id}>{q?.hora}</p>
 		    </>
 	    ))}
 	    </div>
         </div>
-    )
+    )*/
+
+  return (
+    <div className={classes.layout}>
+      <div className={classes.box} style={{ backgroundColor: "yellow" }}>
+        {resultQuarta
+          .filter((q) => q.pars === 1)
+          .map((q) => (
+            <>
+              <p
+                className={classes.primary_text}
+                key={`
+				    id_${q?.id}`}
+              >
+                {q?.titulum}
+              </p>
+              <p key={q?.id}>{q?.hora}</p>
+            </>
+          ))}
+      </div>
+      <div className={classes.box} style={{ backgroundColor: "green" }}></div>
+      <div className={classes.box} style={{ backgroundColor: "red" }}></div>
+      <div className={classes.box} style={{ backgroundColor: "blue" }}></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
